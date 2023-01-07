17dd35b69ed328c456eea82eb5888480525ef740
OlgaJak <jakimenko83@mail.ru>
2023-01-07 16:34:14 +0200

    first version

--- src/DatesClass.java
+++ src/DatesClass.java
@@ -1,35 +1,33 @@
 import java.util.Scanner;
 
 public class DatesClass {
-    public int day;
-    public int month;
-    public int year;
+    public Integer day;
+    public Integer month;
+    public Integer year;
 
-    public DatesClass(int day, int month, int year) {
+
+    public DatesClass(Integer day, Integer month, Integer year) {
         this.day = day;
         this.month = month;
         this.year = year;
+
     }
 
-    public void  americanFormat (int month,int day, int year) {
 
+    public void americanFormat(int month, int day, int year) {
         System.out.println(month + "/" + day + "/" + year);
-    }
-
-    public void europeanFormat (int day, int month, int year) {
-
         System.out.println(day + "/" + month + "/" + year);
     }
 
-//    public void convertation (){
-////        public void convertation (int americanFormat, int europeanFormat){
-//        if ((americanFormat()) == true) {
-//            System.out.println(europeanFormat());
-//        }
-//    }
+    public void europeanFormat(int day, int month, int year) {
+
+        System.out.println(day + "/" + month + "/" + year);
+        System.out.println(month + "/" + day + "/" + year);
+    }
+
 
     public static void main(String[] args) {
-        Scanner scanner  = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
 
         System.out.println("Please enter day: ");
         int day = Integer.valueOf(scanner.nextInt());
@@ -41,11 +39,26 @@ public class DatesClass {
         int year = Integer.valueOf(scanner.nextInt());
 
         System.out.println("Please enter date format: american or european: ");
-        String userInput = scanner.nextLine();
+        Scanner scanner1 = new Scanner(System.in);
+        String userInput = scanner1.nextLine();
 
-        System.out.println("The opposite format will be: ");
+        DatesClass amFormat = new DatesClass(0, 0, 0);
+        DatesClass euFormat = new DatesClass(0, 0, 0);
 
 
+
+        if (userInput.equals("american")) {
+            amFormat.americanFormat(month, day, year);
+
+        } else {
+            euFormat.europeanFormat(day, month, year);
+        }
+
+
+
+
+
+//        System.out.println("The opposite format will be: ");
 
 
     }
